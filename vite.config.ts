d17765a6a7147fca6a09162779d16a8974ee3cbe
andressimus <andrey.pervyakov@gmail.com>
2025-10-27 16:24:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: true,
-    allowedHosts: ['*.csb.app', '*.codesandbox.io'] // ← важно для CSB
-  }
-})
+ server: {
+  port: 3000,
+  host: true,
+  strictPort: true,
+  allowedHosts: ['.csb.app', '.codesandbox.io'] // ← правильно для всех поддоменов CSB
+}
