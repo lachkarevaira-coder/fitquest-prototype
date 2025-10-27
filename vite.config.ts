b1632310873d2ffa3cf34e17d71aff5e1faaf6c2
andressimus <andrey.pervyakov@gmail.com>
2025-10-27 19:11:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
   server: {
-  host: true,
-  port: 5173,
-  strictPort: true,
-  allowedHosts: [/\.csb\.app$/, /\.codesandbox\.io$/],
-  hmr: { clientPort: 443 } // без host, чтобы не залипало на старом домене
-}
-
+    host: true,
+    port: 3000,
+    strictPort: true,
+    allowedHosts: [/\.csb\.app$/, /\.codesandbox\.io$/, 'localhost', '127.0.0.1'],
+    hmr: { clientPort: 443 }
+  }
+})
